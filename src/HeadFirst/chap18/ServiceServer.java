ff16dce7ca446d1332199817cf3576cf26f1ab07
Vladimir <adeptius@gmail.com>
2016-08-07 18:49:55 +0300

    Clearing

--- src/HeadFirst/chap18/ServiceServer.java
+++
@@ -1,9 +0,0 @@
-package HeadFirst.chap18;
-import java.rmi.*;
-
-public interface ServiceServer extends Remote {
-
-    Object[] getServiceList() throws RemoteException;
-
-    Service getService(Object serviceKey) throws RemoteException;
-}
